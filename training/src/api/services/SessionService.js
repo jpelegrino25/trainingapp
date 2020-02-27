4c8d98c6103dcd259a13072b1657775687aea819
Julio Luis Pelegrino <jpelegrino@gmail.com>
2020-02-27 17:00:54 -0400

    Front session crud.

---
+++ training/src/api/services/SessionService.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import * as Constant from '../../Constant.js'
+
+const URI='sessions/'
+
+class SessionService {
+
+    findAll=()=> {
+        return axios.get(Constant.URL_BASE+URI);
+    }
+
+    findById=(sessionId)=> {
+        return axios.get(Constant.URL_BASE+URI+sessionId);
+    }
+
+    save=(session)=> {
+        return axios.post(Constant.URL_BASE+URI,session);
+    }
+
+    update=(session)=> {
+        return axios.put(Constant.URL_BASE+URI,session);
+    }
+
+    delete=(sessionId)=> {
+        return axios.delete(Constant.URL_BASE+URI+sessionId);
+    }
+
+}
+
+export default new SessionService();
